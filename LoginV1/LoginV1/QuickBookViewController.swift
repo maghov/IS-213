fabc114174b1b07e5f8b2ba5757a5edfc5c982e0
eiriho15 <oliversen95@gmail.com>
2017-05-31 18:30:43 +0200

    dropdown menu for quickBookViewController

---
+++ LoginV1/LoginV1/QuickBookViewController.swift
@@ -0,0 +1,45 @@
+//
+//  QuickBookViewController.swift
+//  LoginV1
+//
+//  Created by Eirik Oliversen on 31.05.2017.
+//  Copyright © 2017 eoliversen. All rights reserved.
+//
+
+import UIKit
+import  FirebaseAuth
+
+class QuickBookViewController: UIViewController {
+
+    @IBOutlet weak var viewDropDownMenu: UIView!
+    
+    @IBOutlet weak var buttonLogout: UIButton!
+    @IBAction func showDropDownMenu(_ sender: UIBarButtonItem) {
+        
+        if (viewDropDownMenu.isHidden == true){
+            viewDropDownMenu.isHidden = false
+        }
+        else if (viewDropDownMenu.isHidden == false){
+            viewDropDownMenu.isHidden = true
+        }
+    }
+    
+    @IBAction func logoutButtonPressed(_ sender: UIButton) {
+        try! FIRAuth.auth()?.signOut()
+        performSegue(withIdentifier: "quickBookToLogin", sender: self)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewDropDownMenu.isHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
