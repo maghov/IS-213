8a3b5657e32f48d9947853af7a282be7606468b3
Mohammad Hussain <mo.huss2@gmail.com>
2017-05-31 11:31:39 +0200

    Added support for force touch(quick actions)

--- LoginV1/LoginV1/AppDelegate.swift
+++ LoginV1/LoginV1/AppDelegate.swift
@@ -46,6 +46,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if shortcutItem.type == "MagnusHovik.RoomBooking3.chooseBuilding" {
+            print("test")
+        }
+    }
 
 }
 
